70fc30ec4078ce4fc248c36d7d2f1d65fe86038c
Temporary95Name <128bitprocessor@gmail.com>
2026-03-15 01:44:48 +0000

    salvando

--- ed/pintando/go/draft.go
+++ ed/pintando/go/draft.go
@@ -1,5 +1,43 @@
 package main
-import "fmt"
+
+import (
+    "fmt"
+    "math"
+)
+
 func main() {
-    fmt.Println("qxcode")
+	var a float64
+	var b float64
+	var c float64
+    // var p float64
+    // var area float64
+
+
+    _, err := fmt.Scanln(&a)
+    if err != nil {
+        fmt.Println("Error")
+
+    }
+
+
+    _, err = fmt.Scanln(&b)
+    if err != nil {
+        fmt.Println("Error")
+
+    }
+
+
+    _, err = fmt.Scanln(&c)
+    if err != nil {
+        fmt.Println("Error")
+    }
+
+
+    // p = (a+b+c)/2
+
+    // area = p * (p - a) * (p - b) * (p - c)
+
+    
+    // fmt.Printf("%.2f\n", math.Sqrt(p * (p - a) * (p - b) * (p - c)))
+    fmt.Printf("%.2f\n", math.Sqrt(((a+b+c)/2) * (((a+b+c)/2) - a) * (((a+b+c)/2) - b) * (((a+b+c)/2) - c)))
 }
