923646f2f68fe2bb3f503bcc5a92c9b46b85c849
Temporary95Name <128bitprocessor@gmail.com>
2026-03-21 16:57:31 +0000

    salvando

--- ed/casais/go/draft.go
+++ ed/casais/go/draft.go
@@ -1,5 +1,25 @@
+// Código feito e respondido pelo professor 18 de Março de 2026
 package main
+
 import "fmt"
+
 func main() {
-    fmt.Println("qxcode")
+	qtd := 0
+	fmt.Scan(&qtd)
+	animais := make([]int, qtd)
+
+	solteiros := make(map[int]int)
+	for i := range animais {
+		fmt.Scan(&animais[i])
+	}
+	n_pares := 0
+	for _, animal := range animais {
+		if solteiros[-animal] > 0 {
+			solteiros[-animal]--
+			n_pares++
+		} else {
+			solteiros[animal]++
+		}
+	}
+	fmt.Println(n_pares)
 }
