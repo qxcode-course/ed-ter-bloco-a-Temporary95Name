699fe351ff2650641bd7b1e05bcc72007f4d0aaf
Temporary95Name <128bitprocessor@gmail.com>
2026-03-23 01:21:38 +0000

    salvando

--- ed/fuga/go/draft.go
+++ ed/fuga/go/draft.go
@@ -1,5 +1,41 @@
 package main
+
 import "fmt"
+
+// maisProximo vai de "f" até "pontoFinal" e conta os passos
+func maisProximo(pontoComeco, pontoFinal, direcao int) int {
+	passos := 0
+	atual := pontoComeco
+
+	// Continuar o loop até chegar ao "pontoFinal"
+	for atual != pontoFinal {
+		atual = atual + direcao
+		passos++
+
+		// Fazer com que o loop fique "circular"
+		if atual > 15 {
+			atual = 0
+		} else if atual < 0 {
+			atual = 15
+		}
+	}
+	return passos
+}
+
 func main() {
-    fmt.Println("qxcode")
+	var h, p, f, d int
+
+	// Ler h, p, f, d
+	fmt.Scan(&h, &p, &f, &d)
+
+	// Calcular as distancia
+	distanciaDeA := maisProximo(f, h, d)
+	distanciaDeB := maisProximo(f, p, d)
+
+	// Comparar os resultados e imprimir a resposta
+	if distanciaDeA <= distanciaDeB {
+		fmt.Println("S")
+	} else {
+		fmt.Println("N")
+	}
 }
