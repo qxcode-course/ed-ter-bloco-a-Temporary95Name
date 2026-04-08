9736c603ea815e0aa3451fcfd7fa91d51b30de8a
Temporary95Name <128bitprocessor@gmail.com>
2026-04-08 01:14:09 +0000

    salvando

--- ed/enesimo/go/draft.go
+++ ed/enesimo/go/draft.go
@@ -1,5 +1,44 @@
 package main
+
 import "fmt"
+
+func eh_primo(numero, divisor int) bool {
+	
+	if numero <= 1 {
+		fmt.Println("O numero e igual a 1.")
+		return false
+	}
+	
+	if divisor == numero {
+		return true
+	}
+	
+	if numero % divisor == 0 {
+		return false
+	}
+	
+	return eh_primo(numero, divisor + 1)
+}
+
+func enesimoPrimo(num1 int, num2 int, contagem int) int {
+	
+	if eh_primo(num2, 2) {
+		contagem++
+	}
+	
+	if contagem == num1 {
+		return num2
+	}
+	
+	return enesimoPrimo(num1, num2 + 1, contagem)
+}
+
 func main() {
-    fmt.Println("qxcode")
+	var num int
+	
+	fmt.Scanln(&num)
+	
+	result := enesimoPrimo(num, 2, 0)
+	
+	fmt.Println(result)
 }
