6001ce60b9b1de81a6dbe6357c1ad5d16618f105
Temporary95Name <128bitprocessor@gmail.com>
2026-04-14 14:17:26 +0000

    salvando

--- eval1/apagando/src/go/draft.go
+++ eval1/apagando/src/go/draft.go
@@ -1,4 +1,40 @@
 package main
+
 import "fmt"
+
+func removeCommonSet(vet1, vet2 []int) []int {
+
+	aux := make([]int, 0, len(vet1))
+
+	for i := 0; i < len(vet1); i++ {
+		for j := 0; j < len(vet2); j++ {
+			if vet1[i] != vet2[j] {
+				vet1[i] = vet2[j]
+			} else {
+				continue
+			}
+		}
+	}
+
+	return aux
+}
+
 func main() {
+	var num1 int
+	var num2 int
+
+	fmt.Scanln(&num1)
+	vet1 := make([]int, 0, num1)
+	for i := 0; i < num1; i++ {
+		fmt.Scanln(vet1[i])
+	}
+
+	fmt.Scanln(&num2)
+	vet2 := make([]int, 0, num2)
+	for i := 0; i < num2; i++ {
+		fmt.Scanln(vet2[i])
+	}
+
+	resultado := removeCommonSet(vet1, vet2)
+	fmt.Println(resultado)
 }
