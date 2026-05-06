d912bc54c0baf9a5a2dcf3bbcd59af3bb76947a8
Temporary95Name <128bitprocessor@gmail.com>
2026-05-06 14:39:54 +0000

    salvando

--- ed/sufixos/go/draft.go
+++ ed/sufixos/go/draft.go
@@ -1,5 +1,28 @@
 package main
 import "fmt"
+
+
+func printRecursaoPiramide(texto string) {
+
+    if len(texto) == 0 {
+        return
+    } 
+
+    // Chamada recursiva
+    printRecursaoPiramide(texto[1:len(texto)])
+    
+    // Armazenar o resultado na stack, para imprimir os resultados em ordem invertida
+    fmt.Println(texto)
+
+}
+
+
 func main() {
-    fmt.Println("qxcode")
+    
+    var texto string
+
+    fmt.Scanln(&texto)
+
+    printRecursaoPiramide(texto)
+    
 }
