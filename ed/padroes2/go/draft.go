c34a1b7921bd6fe6f58075cf2bcf0d9ffbed67f9
Temporary95Name <128bitprocessor@gmail.com>
2026-05-10 01:43:09 +0000

    salvando

--- ed/padroes2/go/draft.go
+++ ed/padroes2/go/draft.go
@@ -1,6 +1,17 @@
 // equacao para a resposta e: (n elevado a 2) + (n vezes 2)
+// Ainda nao usa recursao
 package main
+
 import "fmt"
+
 func main() {
-    fmt.Println("qxcode")
+
+	var n int
+
+	fmt.Scanln(&n)
+
+	// resultado := (n*n)+(n*2)
+
+	fmt.Printf("%d\n", (n*n)+(n*2))
+	// fmt.Printf("\n")
 }
