99d6417b3021ea1d04fe7b0310e6d39ff5c5525a
Temporary95Name <128bitprocessor@gmail.com>
2026-06-08 01:28:07 +0000

    salvando

--- ed/lista_d1/go/draft.go
+++ ed/lista_d1/go/draft.go
@@ -1,5 +1,22 @@
+// ed man s09e01 lista
 package main
+
 import "fmt"
+
+type Node struct {
+	info int
+	next *Node
+	prev *Node
+}
+
+/**/
+
+type DList struct {
+	head *Node
+}
+
+/**/
+
 func main() {
-    fmt.Println("qxcode")
+	fmt.Println("qxcode")
 }
